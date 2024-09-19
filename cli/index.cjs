86006151ba57ffde8613279af96e2fb542e1d158
wiw <81812572+williamcostello@users.noreply.github.com>
2024-09-19 23:43:59 +0100

    Feature/v1.2.0 (#3)
    * Feature/global instanced choice (#1)
    
    * !feat: Templates for Instanced and Global mode
    
    * feat: Copy selected mode template - fixes: previous breaking change
    
    * feat: Colorise the success output (#2)
    
    * feat: Bump minor version number

--- cli/index.cjs
+++ cli/index.cjs
@@ -1,17 +1,34 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process')
-const { input, confirm } = require('@inquirer/prompts')
+const { input, confirm, select } = require('@inquirer/prompts')
+const chalk = require('chalk')
 const fs = require('fs')
 const path = require('path')
-
-const templateDir = path.resolve(__dirname, 'template')
 
 const main = async () => {
     const answers = {
         projectName: await input({
             message: 'Enter the project name:',
-            default: 'p5-ts-starter',
+            default: 'p5-ts-startser',
+        }),
+        mode: await select({
+            message: 'Select the mode:',
+            choices: [
+                {
+                    title: 'Global',
+                    value: 'global',
+                    description:
+                        'Uses the same syntax as the p5 web editor. Great for prototyping. HMR is disabled for this mode',
+                },
+                {
+                    title: 'Instanced',
+                    value: 'instanced',
+                    description:
+                        'Use this if the sketch will not be used in isolation.',
+                },
+            ],
+            default: 'global',
         }),
         gitInit: await confirm({
             message: 'Initialize a git repository?',
@@ -44,6 +61,8 @@ const main = async () => {
         console.error('Failed to create project directory:', error.message)
         process.exit(1)
     }
+
+    const templateDir = path.join(__dirname, 'template', answers.mode)
 
     try {
         fs.cpSync(templateDir, projectPath, {
@@ -80,14 +99,27 @@ const main = async () => {
             cleanup()
             process.exit(1)
         }
+    } else {
+        try {
+            fs.rmSync(path.join(projectPath, '.gitignore'))
+        } catch (error) {
+            console.error('Failed to remove .gitignore:', error.message)
+            cleanup()
+            process.exit(1)
+        }
     }
 
-    console.info(`Project ${answers.projectName} is ready!`)
     console.info(
-        `run 'cd ${answers.projectName}' to enter the project directory
-run 'npm install' to install dependencies
-run 'npm run dev' to start the development server`
+        chalk.greenBright(
+            'Project',
+            chalk.bold(`"${answers.projectName}"`),
+            'is ready!'
+        )
     )
+    console.info(chalk.bold(chalk.blueBright(`Next steps:`)))
+    console.info(chalk.yellowBright(`cd "${answers.projectName}"`))
+    console.info(chalk.yellowBright(`yarn install`))
+    console.info(chalk.yellowBright(`yarn dev`))
 }
 
 main()
