784362df8821132163e81a15d28135854c3690ce
williamcostello <william.costello@mattressonline.co.uk>
2024-09-22 14:22:40 +0100

    fix: Remove global mode import for instanced mode

--- cli/template/instanced/global.d.ts
+++ cli/template/instanced/global.d.ts
@@ -2,7 +2,6 @@
  * These declarations give you type checking and intellisense in both global and instance mode
  */
 
-import * as p5Global from 'p5/global'
 import p5 from 'p5'
 export = p5
 export as namespace p5
